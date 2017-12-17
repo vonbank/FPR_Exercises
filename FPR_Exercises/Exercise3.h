95a9d52f904d999b1f39a5249890c37a06d0b954
Philip Burggasser <cool-phil15@hotmail.com>
2017-12-17 10:09:10 +0100

    Update Exercise3.h

--- FPR_Exercises/Exercise3.h
+++ FPR_Exercises/Exercise3.h
@@ -7,8 +7,9 @@ std::vector<T> scan(T(*f)(T, T), T initialScanValue, std::vector<T> input)
 {
 	if (input.size() > 1)
 	{
-		std::vector<T> tmp = scan(f, f(initialScanValue, input.front()), std::vector<T>(input.begin() + 1, input.end()));
-		std::vector<T> output = { f(initialScanValue, input.front()) };
+		T calculatedValue = f(initialScanValue, input.front());
+		std::vector<T> tmp = scan(f, calculatedValue, std::vector<T>(input.begin() + 1, input.end()));
+		std::vector<T> output = {};
 		output.insert(output.end(), tmp.begin(), tmp.end());
 		return output;
 	}
